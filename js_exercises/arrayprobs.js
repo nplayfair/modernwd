781574e0b3ee59203ed8865426b9d30795f220a6
Nick Playfair <nick@chaosemerald.co.uk>
2017-05-15 14:22:11 +0100

    2nd array task

---
+++ js_exercises/arrayprobs.js
@@ -0,0 +1,22 @@
+var fruits = ["Apple", "Banana", "Orange", "Pear"];
+var veg = ["Carrot", "Carrot", "Carrot", "Carrot"]
+
+
+function printReverse(array) {
+	var numElements =  array.length -1;
+	for (var i = numElements; i >= 0; i--) {
+		console.log(i + " " + array[i]);
+	}
+}
+
+function isUniform(array) {
+	var uniform = false;
+	var firstElement = array[0];
+	array.forEach(function(element) {
+		if (element === firstElement) {
+			uniform  = true;
+		} 
+		else (uniform = false)
+	});
+	return uniform;
+}
--- js_exercises/list.js
+++ js_exercises/list.js
@@ -6,25 +6,39 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
 	//handle input
 	if (input === "list") {
-		console.log("********");
-		todos.forEach(function(todo, index){
-			console.log(index + ": " + todo);
-		});
-		console.log("********");
+		listTodos();
 } else if (input === "new") {
 	//ask for new
+	addTodo();
+} else if (input === "delete") {
+	deleteTodo();
+}
+	//ask again
+	var input = prompt("What would you like to do?");
+}
+console.log("OK bye!");
+
+
+// Functions
+
+function listTodos() {
+	console.log("********");
+	todos.forEach(function(todo, index){
+		console.log(index + ": " + todo);
+	});
+	console.log("********");
+}
+
+function addTodo() {
 	var newTodo = prompt("Enter new entry");
 	todos.push(newTodo);
 	console.log("Added item");
-} else if (input === "delete") {
+}
+
+function deleteTodo() {
 	//ask for index for deletion
 	var index = prompt("Enter index to delete:");
 	//delete item
 	todos.splice(index, 1);
 	console.log("Deleted item");
 }
-
-	//ask again
-	var input = prompt("What would you like to do?");
-}
-console.log("OK bye!");
