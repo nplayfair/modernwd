25e671872b228221be25a810304fa44c64efa1d2
Nick Playfair <nick@chaosemerald.co.uk>
2018-05-10 16:52:50 +0100

    event listeners

--- dom/dom.js
+++ dom/dom.js
@@ -1,2 +1,7 @@
 var h1 = document.querySelector("h1");
 h1.style.color = "red";
+var paragraph = document.querySelector("#buttonstatus");
+
+document.querySelector("button").addEventListener("click", function() {
+  paragraph.textContent = "Somebody clicked!";
+});
