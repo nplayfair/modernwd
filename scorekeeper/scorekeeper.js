df25a50832f6c72dadd9d6c82d43c80a2e4df8f8
Nick Playfair <nick@chaosemerald.co.uk>
2018-05-11 15:01:14 +0100

    Reset button event

--- scorekeeper/scorekeeper.js
+++ scorekeeper/scorekeeper.js
@@ -2,6 +2,7 @@
 
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 
@@ -18,6 +19,8 @@ p1Button.addEventListener("click", function() {
   if (!gameOver) {
     p1Score++;
     if (p1Score === winningScore) {
+      //Player 1 has won
+      p1Display.classList.add("winner");
       gameOver = true;
       console.log("Game Over!");
     }
@@ -29,9 +32,21 @@ p2Button.addEventListener("click", function() {
   if (!gameOver) {
     p2Score++;
     if (p2Score === winningScore) {
+      //Player 2 has won
+      p2Display.classList.add("winner");
       gameOver = true;
       console.log("Game Over!");
     }
     p2Display.textContent = p2Score;
   }
+});
+
+resetButton.addEventListener("click", function() {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
 });
