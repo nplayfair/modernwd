9312aa51a1c5bf3d324cd129caaf1a60ecbc8167
Nick Playfair <nick@chaosemerald.co.uk>
2018-05-12 16:55:45 +0100

    basic game logic implemented

--- rgbgame/rgb.js
+++ rgbgame/rgb.js
@@ -9,11 +9,29 @@ var colors = ["rgb(255, 0, 0)",
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
-//Set square colors
+//Set square colors and events
 
 for (var i = 0; i < squares.length; i++) {
+  //Add initial colors
   squares[i].style.backgroundColor = colors[i];
+  //Add listeners
+  squares[i].addEventListener("click", function() {
+    //Get color of clicked square
+    var clickedColor = this.style.backgroundColor;
+    //Compare color to pickedColor
+    if (clickedColor === pickedColor) {
+      //Correct
+      messageDisplay.textContent = "Correct!";
+    }
+    else {
+      //Incorrect
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
+    }
+  });
+
 }
