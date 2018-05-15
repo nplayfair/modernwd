20d2c2621a3feee9a7ce2879ea5df34d34b75316
Nick Playfair <nick@chaosemerald.co.uk>
2018-05-15 19:02:39 +0100

    Delete item by clicking the X

--- todo_app/assets/js/todo.js
+++ todo_app/assets/js/todo.js
@@ -2,3 +2,13 @@
 $("li").on("click", function() {
   $(this).toggleClass("completed");
 });
+
+//Delete item by clicked the delete button
+$("li span").on("click", function(event) {
+  //Remove the parent li
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  //Stop event from propagating up the DOM
+  event.stopPropagation();
+});
