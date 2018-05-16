025b956f8d3e52f6b04d5f31501ed12b0487f65c
Nick Playfair <nick@chaosemerald.co.uk>
2018-05-16 17:23:11 +0100

    Add some routes

--- backend/FirstExpress/app.js
+++ backend/FirstExpress/app.js
@@ -1,1 +1,22 @@
 var express = require("express");
+var app = express();
+
+// "/" => Hi
+app.get("/", function(req, res) {
+  res.send("Hi there!");
+});
+
+// "/bye" => Goodbye
+app.get("/bye", function(req, res){
+  res.send("Goodbye!");
+});
+
+// "/dog" => woof
+app.get("/dog", function(req, res){
+  res.send("Woof!");
+});
+
+//Tell express to listen for requests
+app.listen(3000, function(){
+  console.log("Server started.");
+});
