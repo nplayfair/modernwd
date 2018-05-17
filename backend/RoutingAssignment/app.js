5fcc4c439121d6cf88e54d52d5e583d58462e3d2
Nick Playfair <nick@chaosemerald.co.uk>
2018-05-17 16:06:49 +0100

    Route logic

---
+++ backend/RoutingAssignment/app.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var app = express();
+
+//root route
+app.get("/", function(req, res) {
+  res.send("Hi there, welcome to my assignment.");
+});
+
+app.get("/speak/:animal", function(req, res) {
+  //Animal sounds
+  var animal = req.params.animal;
+
+  //Switch statement
+  switch(animal) {
+    case "pig":
+    {
+      res.send("Oink");
+      break;
+    }
+    case "cow":
+    {
+      res.send("Moo");
+      break;
+    }
+    case "dog":
+    {
+      res.send("Woof woof!");
+      break;
+    }
+    default:
+    {
+      res.send("Page not found");
+    }
+  }
+});
+
+app.get("/repeat/:word/:num", function(req, res) {
+  //repeat words
+  var word = req.params.word;
+  var numRepeats = Number(req.params.num);
+  var output = "";
+
+  for (var i = 0; i < numRepeats; i++) {
+    output += word + " ";
+  }
+  res.send(output);
+});
+
+//* route
+app.get("*", function(req, res) {
+  res.send("Page not found");
+});
+
+
+//Tell express to listen for requests
+app.listen(3000, function(){
+  console.log("Server started.");
+});
