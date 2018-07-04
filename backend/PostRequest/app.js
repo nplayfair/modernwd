aa62647949b5979ff4568d8e0cf6aab6abe7341d
Nick Playfair <nplayfair@protonmail.com>
2018-07-04 15:56:04 +0100

    Add new element by sending a post request via a form

--- backend/PostRequest/app.js
+++ backend/PostRequest/app.js
@@ -1,14 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+var friends = ["Tony", "Sophia", "Tom", "Greg", "Sarita"];
 
 app.get("/", function(req, res) {
   res.render("home");
 });
 
+app.post("/addfriend", function(req, res) {
+  var newFriend = req.body.newfriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
+});
+
 app.get("/friends", function(req, res) {
-  var friends = ["Tony", "Sophia", "Tom", "Greg", "Sarita"];
+
   res.render("friends",{friends: friends});
 });
 
