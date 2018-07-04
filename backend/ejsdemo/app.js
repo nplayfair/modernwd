2251a6a73d10f2e093843e332a1369299248dc7f
Nick Playfair <nplayfair@protonmail.com>
2018-07-04 14:36:20 +0100

    Create array of posts and display them in a view

--- backend/ejsdemo/app.js
+++ backend/ejsdemo/app.js
@@ -11,6 +11,17 @@ app.get("/sayhelloto/:thing", function(req, res) {
   res.render("sayhello.ejs", {thingVar: thing}); //pass var to template
 });
 
+app.get("/posts", function(req, res) {
+  //Define array of posts
+  var posts = [
+    {title: "Nottingham", author: "Nick"},
+    {title: "Founding Apple", author: "Steve"},
+    {title: "World Tour", author: "Ashley"}
+  ];
+
+  res.render("posts.ejs", {posts: posts}); //pass array of posts to view
+});
+
 //Tell express to listen for requests
 app.listen(3000, function(){
   console.log("Server started.");
