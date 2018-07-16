f36498ac7af93a02076398cbe816f85f44b5cc5d
Nick Playfair <nick@chaosemerald.co.uk>
2018-07-16 14:39:24 +0100

    add secret route and db connection

--- authtest/app.js
+++ authtest/app.js
@@ -1,12 +1,17 @@
 var express =  require("express");
+var mongoose =  require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/authdemo", {useNewUrlParser: true});
 
 var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res) {
   res.render("home");
-})
+});
 
+app.get("/secret", function(req,res) {
+  res.render("secret");
+});
 
 //Tell express to listen for requests
 app.listen(3000, function(){
